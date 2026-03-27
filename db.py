d2c47fe3aad729c7bb6baa550774e251c0acf8af
kanakadurga <kanakadurgapitchuka999@gmail.com>
2026-03-27 15:37:40 +0530

    Refactor db.py with comments for clarity
    Added comments to clarify client assignment and database structure.

--- db.py
+++ db.py
@@ -1,8 +1,12 @@
 from pymongo import MongoClient
 
-MongoClient("mongodb+srv://kanakadurgapitchuka999_db_user:UPsfxBaDDHOpKDc5@cluster0.gxymtsz.mongodb.net/")
+# ✅ client ni assign cheyali
+client = MongoClient("mongodb+srv://kanakadurgapitchuka999_db_user:UPsfxBaDDHOpKDc5@cluster0.gxymtsz.mongodb.net/")
+
+# database
 db = client["ai_expense_tracker"]
 
+# collections
 users_col = db["users"]
 expenses_col = db["expenses"]
-budgets_col = db["budgets"]   
+budgets_col = db["budgets"]
