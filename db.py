6005dacb2a55f040997cd9491d310c59029fa211
kanakadurga <kanakadurgapitchuka999@gmail.com>
2026-03-27 21:47:55 +0530

    Enhance MongoDB client with timeout setting
    Added server selection timeout to MongoDB client.

--- db.py
+++ db.py
@@ -1,6 +1,9 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://kanakadurgapitchuka999_db_user:UPsfxBaDDHOpKDc5@cluster0.gxymtsz.mongodb.net/ai_expense_tracker?retryWrites=true&w=majority")
+client = MongoClient(
+    "mongodb+srv://kanakadurgapitchuka999_db_user:UPsfxBaDDHOpKDc5@cluster0.gxymtsz.mongodb.net/ai_expense_tracker?retryWrites=true&w=majority",
+    serverSelectionTimeoutMS=5000  # 🔥 important
+)
 
 db = client["ai_expense_tracker"]
 
