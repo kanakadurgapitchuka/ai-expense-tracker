97606170e04f45a8df5308d04247913c2d58b8a2
kanakadurga <kanakadurgapitchuka999@gmail.com>
2026-03-27 15:50:07 +0530

    Remove unnecessary comments in db.py

--- db.py
+++ db.py
@@ -1,12 +1,9 @@
 from pymongo import MongoClient
 
-# ✅ client ni assign cheyali
 client = MongoClient("mongodb+srv://kanakadurgapitchuka999_db_user:UPsfxBaDDHOpKDc5@cluster0.gxymtsz.mongodb.net/")
 
-# database
 db = client["ai_expense_tracker"]
 
-# collections
 users_col = db["users"]
 expenses_col = db["expenses"]
 budgets_col = db["budgets"]
